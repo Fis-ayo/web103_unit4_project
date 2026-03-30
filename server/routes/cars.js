a8b4d22d3fd82bfb7d237839ec63715fbc260455
Mercy Akinyemi <akinyemimercy293@gmail.com>
2026-03-29 23:19:04 -0500

    fix: change PUT to PATCH for updating car details in routes

--- server/routes/cars.js
+++ server/routes/cars.js
@@ -3,10 +3,11 @@ import carController from '../controllers/cars.js'
 
 const router = express.Router();
 
+router.get('/options', carController.getAllOptions);
 router.get('/cars', carController.getAllCars);
 router.get('/cars/:id', carController.getCarById);
 router.post('/cars', carController.createCar);
-router.put('/cars/:id', carController.updateCar);
+router.patch('/cars/:id', carController.updateCar);
 router.delete('/cars/:id', carController.deleteCar);
 
 export default router;
